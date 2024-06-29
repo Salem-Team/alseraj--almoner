8b3ec4ee3a5b2810ac8ef7360cb3bc56c11ba8e3
Salem-Team <salemteamcompany@gmail.com>
2024-06-30 01:37:13 +0300

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdk3sqIHjXvB2C-O-lvkRgMFpg8pemkno",
+  authDomain: "alseraj--almoner.firebaseapp.com",
+  projectId: "alseraj--almoner",
+  storageBucket: "alseraj--almoner.appspot.com",
+  messagingSenderId: "462211256149",
+  appId: "1:462211256149:web:a03ace3c70b306620169dc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+app;
