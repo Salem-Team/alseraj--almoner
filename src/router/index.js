abace4fbe8c33a7a5cf2d6e245def9070d3fb249
Eslam Abosife <156938103+eslam1abosife@users.noreply.github.com>
2024-07-02 17:23:54 +0300

    start

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Home from "../views/HomeView.vue";
 import ClassRoom from "../views/Class_room.vue";
 import ClassPage from "../views/classs_page.vue";
 import TheAdministrator from "../views/TheAdministrator.vue";
+import UserLogin from "../views/USerLogin.vue";
+import Parent_Dashboard from "../views/Parent_Dashboard.vue";
+import Student_Dashboard from "../views/Student_Dashboard.vue";
+import admin_Dashboard from "../views/admin_Dashboard.vue";
+import Edit_profile from "../views/Edit_profile.vue";
 
 const routes = [
   {
@@ -27,7 +32,32 @@ const routes = [
     name: "Admin",
     component: TheAdministrator,
   },
-  // يمكنك إضافة المزيد من المسارات هنا إذا لزم الأمر
+  {
+    // يمكنك إضافة المزيد من المسارات هنا إذا لزم الأمر
+    path: "/UserLogin",
+    name: "UserLogin",
+    component: UserLogin,
+  },
+  {
+    path: "/Parent_Dashboard",
+    name: "Parent_Dashboard",
+    component: Parent_Dashboard,
+  },
+  {
+    path: "/Student_Dashboard",
+    name: "Student_Dashboard",
+    component: Student_Dashboard,
+  },
+  {
+    path: "/admin_Dashboard",
+    name: "admin_Dashboard",
+    component: admin_Dashboard,
+  },
+  {
+    path: "/Edit_profile",
+    name: "Edit_profile",
+    component: Edit_profile,
+  },
 ];
 
 const router = createRouter({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
-
 <script>
 // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    HelloWorld,
+  },
 };
 </script>
+
+<style lang="scss"></style>
