2acdb235f03cc838b5fe0a72cafb9dc969895df1
Eslam Abosife <156938103+eslam1abosife@users.noreply.github.com>
2024-07-02 17:16:15 +0300

    start

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+// import store from "./store";
+import { createPinia } from "pinia";
 
-createApp(App).use(store).use(router).mount("#app");
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css"; // أو استيراد أيقونات أخرى إن كنت تفضل ذلك
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(createPinia()).use(vuetify).use(router).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "../views/HomeView.vue";
+import ClassRoom from "../views/Class_room.vue";
+import ClassPage from "../views/classs_page.vue";
+import TheAdministrator from "../views/TheAdministrator.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "Home",
+    component: Home,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/ClassRoom",
+    name: "Class",
+    component: ClassRoom,
+    props: true,
   },
+  {
+    path: "/class/:year",
+    name: "classPage",
+    component: ClassPage,
+    props: true,
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: TheAdministrator,
+  },
+  // يمكنك إضافة المزيد من المسارات هنا إذا لزم الأمر
 ];
 
 const router = createRouter({
