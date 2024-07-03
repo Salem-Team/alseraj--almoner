793ff5c0501b4ab880f2098634f0bdf15ddd8b23
Salem Team <159494039+Salem-Team@users.noreply.github.com>
2024-07-03 21:26:44 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,76 @@ import { useAuthStore } from "./store/userStore";
 import { mapActions, mapState } from "pinia";
 
 export default {
+        data() {
+        return {
+            Students: {
+                id: "jhuhce789394hfp",
+                Student_information: {
+                    student_name: "محمود علي حسين",
+                    class: "3/1",
+                    educational_level: "الصف الثالث الإبتدائي",
+                    gender: "ذكر",
+                    Section: "لغات",
+                    birthday: "2010/3/2",
+                },
+                Results: {
+                    Weekly: [
+                        {
+                            Subject_Name: "دين",
+                            Major_degree: 100,
+                            Student_degree: 96,
+                        },
+                        {
+                            Subject_Name: "دراسات",
+                            Major_degree: 50,
+                            Student_degree: 42,
+                        },
+                    ],
+                    Monthly: [
+                        {
+                            Certificate_title: "شهر نوفمبر",
+                            Degrees: [
+                                {
+                                    Subject_Name: "عربي",
+                                    Teacher_Name: "عماد عمر",
+                                    Behavior_assessment: "ممتاز",
+                                    Minor_degree: 50,
+                                    Major_degree: 100,
+                                    Student_degree: 99,
+                                },
+                                {
+                                    Subject_Name: "قرآن كريم",
+                                    Teacher_Name: "نور محمود",
+                                    Behavior_assessment: "ممتاز",
+                                    Minor_degree: 50,
+                                    Major_degree: 100,
+                                    Student_degree: 95,
+                                },
+                            ],
+                        },
+                    ],
+                },
+                Payments: {
+                    Required: 20000,
+                    Paid_Up: 10000,
+                    Installment_System: 2,
+                },
+                Notifications: [
+                    {
+                        Title: "جواب الفصل لولي الأمر",
+                        Details:
+                            "السيد/السيدة [اسم ولي الأمر]، نود إعلامكم بقرار فصل ابنكم/ابنتكم [اسم الطالب] من معهد السراج المنير الأزهري بسبب تكرار المخالفات للوائح والانضباط المدرسي، وذلك اعتبارًا من تاريخ هذا الخطاب.",
+                    },
+                ],
+                Images: [
+                    {
+                        link: "https://firebasestorage.googleapis.com/v0/b/alseraj--almoner.appspot.com/o/images%2F%D8%AE%D9%84%D9%81%D9%8A%D8%A9%20%D9%84%D9%84%D9%85%D8%B4%D8%B1%D9%88%D8%B9%20-%20Copy.jpg?alt=media&token=6262aa52-b522-47e1-b17d-a11ecec9e4cf",
+                        Date: "2022/3/4",
+                    },
+                ],
+            },
+        };
+    },
     computed: {
         ...mapState(useAuthStore, ["user"]),
     },
